0fff41bdd99a649a1befec0a84b22b9100882388
Alex <droog@pop-os.localdomain>
2020-08-09 13:59:40 -0400

    added defcon scraper

---
+++ defcon_scraper/droog_scraper.py
@@ -0,0 +1,29 @@
+import string
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = 'https://www.slangsite.com/slang/'
+
+words = []
+
+for letter in string.ascii_uppercase:
+    url_req = f'{URL}{letter}.html'
+    page_get = requests.get(url_req)
+    soup = BeautifulSoup(page_get.content, 'html.parser')
+    # words we want are enclosed within a specific size font tag of 3
+    extract = soup.findAll('font',{'size':'3'})
+    #loop to extract text in between font tags
+    for i in extract:
+        # strip away html tags
+        word = i.text
+        # strip away trailing whitespace
+        stripped = word.strip()
+        # strip away colon ':'
+        final_word = stripped.strip(':')
+        words.append(final_word)
+
+word_list = open ('slang_words.txt', 'w')
+for word in words:
+    word_list.write(word + '\n')
+word_list.close()
